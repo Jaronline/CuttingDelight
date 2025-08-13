899abdb561fa8ecf0e5648fe9a6cc4b26c6f273b
Jaron Zijlstra <support@jaronline.dev>
2025-08-13 16:30:33 +0200

    feat: use data generation for recipes

---
+++ src/main/java/dev/jaronline/cuttingdelight/data/DataGenerators.java
@@ -0,0 +1,24 @@
+package dev.jaronline.cuttingdelight.data;
+
+import dev.jaronline.cuttingdelight.CuttingDelight;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.concurrent.CompletableFuture;
+
+@SuppressWarnings("unused")
+@EventBusSubscriber(modid = CuttingDelight.MOD_ID)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput output = generator.getPackOutput();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new Recipes(output, lookupProvider));
+    }
+}
