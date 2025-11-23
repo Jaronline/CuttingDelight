411bd8921d91caba0808ff4ad295f71012fb244b
Jaron Zijlstra <support@jaronline.dev>
2025-11-23 21:37:29 +0100

    feat: process feedback

--- src/main/java/dev/jaronline/cuttingdelight/common/block/entity/CustomCuttingBoardBlockEntity.java
+++ src/main/java/dev/jaronline/cuttingdelight/common/block/entity/CustomCuttingBoardBlockEntity.java
@@ -50,9 +50,10 @@ public class CustomCuttingBoardBlockEntity extends CuttingBoardBlockEntity {
         }
 
         for (ItemStack resultStack : results) {
-            if (player == null || !player.addItem(resultStack)) {
+            ItemStack stackToAdd = resultStack.copy();
+            if (player == null || !player.addItem(stackToAdd)) {
                 Direction direction = getBlockState().getValue(CustomCuttingBoardBlock.FACING).getCounterClockWise();
-                ItemUtils.spawnItemEntity(level, resultStack.copy(),
+                ItemUtils.spawnItemEntity(level, stackToAdd,
                         worldPosition.getX() + 0.5 + (direction.getStepX() * 0.2), worldPosition.getY() + 0.2, worldPosition.getZ() + 0.5 + (direction.getStepZ() * 0.2),
                         direction.getStepX() * 0.2F, 0.0F, direction.getStepZ() * 0.2F);
             }
