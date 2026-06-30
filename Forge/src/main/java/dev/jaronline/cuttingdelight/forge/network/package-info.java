caf6f4aeb5522545489daf8ecbe28470a064f29f
Jaron Zijlstra <support@jaronline.dev>
2026-06-30 12:19:08 +0200

    chore: add missing package-info files

---
+++ Forge/src/main/java/dev/jaronline/cuttingdelight/forge/network/package-info.java
@@ -0,0 +1,8 @@
+@ParametersAreNonnullByDefault
+@FieldsAreNonnullByDefault
+@MethodsAreNonnullByDefault
+package dev.jaronline.cuttingdelight.forge.network;
+
+import dev.jaronline.cuttingdelight.core.FieldsAreNonnullByDefault;
+import dev.jaronline.cuttingdelight.core.MethodsAreNonnullByDefault;
+import dev.jaronline.cuttingdelight.core.ParametersAreNonnullByDefault;
