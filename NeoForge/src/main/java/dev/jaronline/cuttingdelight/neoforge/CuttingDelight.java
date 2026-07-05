4db370e23eb0b560a5da46ce34f742e22afded34
Jaron Zijlstra <support@jaronline.dev>
2026-07-05 22:51:22 +0200

    refactor(ToolCarvingEvent): use custom event adapter (#147)

--- NeoForge/src/main/java/dev/jaronline/cuttingdelight/neoforge/CuttingDelight.java
+++ NeoForge/src/main/java/dev/jaronline/cuttingdelight/neoforge/CuttingDelight.java
@@ -2,11 +2,14 @@ package dev.jaronline.cuttingdelight.neoforge;
 
 import com.mojang.logging.LogUtils;
 import dev.jaronline.cuttingdelight.common.ModIds;
+import dev.jaronline.cuttingdelight.common.block.CustomCuttingBoardBlock;
 import dev.jaronline.cuttingdelight.common.config.ConfigManager;
+import dev.jaronline.cuttingdelight.neoforge.adapter.NeoForgeRightClickBlockEvent;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 @Mod(ModIds.CUTTING_DELIGHT_ID)
@@ -16,5 +19,7 @@ public class CuttingDelight {
 	public CuttingDelight(IEventBus modEventBus, ModContainer modContainer) {
 		ConfigManager.setConfig(new Config());
 		modContainer.registerConfig(ModConfig.Type.SERVER, Config.SPEC);
+		NeoForge.EVENT_BUS.addListener(NeoForgeRightClickBlockEvent.withMethod(CustomCuttingBoardBlock
+				.ToolCarvingEvent::onSneakPlaceTool));
 	}
 }
---
+++ NeoForge/src/main/java/dev/jaronline/cuttingdelight/neoforge/adapter/NeoForgeRightClickBlockEvent.java
@@ -0,0 +1,47 @@
+package dev.jaronline.cuttingdelight.neoforge.adapter;
+
+import dev.jaronline.cuttingdelight.common.event.RightClickBlockEvent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+
+import java.util.function.Consumer;
+
+public class NeoForgeRightClickBlockEvent implements RightClickBlockEvent {
+	private final PlayerInteractEvent.RightClickBlock event;
+
+	private NeoForgeRightClickBlockEvent(PlayerInteractEvent.RightClickBlock event) {
+		this.event = event;
+	}
+
+	public static Consumer<PlayerInteractEvent.RightClickBlock> withMethod(Consumer<RightClickBlockEvent> method) {
+		return event -> method.accept(new NeoForgeRightClickBlockEvent(event));
+	}
+
+	@Override
+	public Level getLevel() {
+		return event.getLevel();
+	}
+
+	@Override
+	public BlockPos getPos() {
+		return event.getPos();
+	}
+
+	@Override
+	public Player getEntity() {
+		return event.getEntity();
+	}
+
+	@Override
+	public void setCanceled(boolean canceled) {
+		event.setCanceled(canceled);
+	}
+
+	@Override
+	public void setCancellationResult(InteractionResult result) {
+		event.setCancellationResult(result);
+	}
+}
