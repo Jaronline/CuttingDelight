0d0c9e31b2e4784963c197274b59235024c869de
Jaron Zijlstra <support@jaronline.dev>
2026-09-10 20:32:52 +0200

    build: change mod publish tokens to environment variables

--- NeoForge/build.gradle.kts
+++ NeoForge/build.gradle.kts
@@ -205,7 +205,7 @@ publishMods {
 
         curseforge {
             projectId = cuttingDelight.curseProjectId
-            accessToken = cuttingDelight.curseforgeApikey
+            accessToken = cuttingDelight.curseforgeApiKey
             changelog.set(changelogHtml.singleFileContents())
             changelogType = "html"
             minecraftVersionRange {
