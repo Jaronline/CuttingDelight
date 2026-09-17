89824c20ab83bbfecef41dc24734bd4cdaefe7f4
Jaron Zijlstra <support@jaronline.dev>
2026-09-17 14:03:21 +0200

    build: add git hooks (#233)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -33,57 +33,24 @@ pluginManagement {
     }
 }
 
-plugins {
-    // https://plugins.gradle.org/plugin/org.danilopianini.gradle-pre-commit-git-hooks
-    id("org.danilopianini.gradle-pre-commit-git-hooks") version ("2.1.24")
-}
+runCatching {
+	logger.info("Configuring git hooks")
 
-gitHooks {
-    preCommit {
-        from("#!/bin/sh") {
-            """
-            echo "*********************************************************"
-            echo "Running git pre-commit hook. Running Spotless Apply... "
-            echo "*********************************************************"
-            
-            stagedFiles=$(git diff --staged --name-only)
-            
-            """
-        }
-        tasks("spotlessApply")
-        appendScript {
-            """
-            status=$?
+	providers.exec {
+		commandLine("git", "config", "extensions.worktreeConfig", "true")
+	}.result.get()
 
-            if [ "${'$'}status" = 0 ] ; then
-                echo "Static analysis found no problems."
-                # Add staged file changes to git
-                for file in ${'$'}stagedFiles; do
-                  if test -f "${'$'}file"; then
-                    git add "${'$'}file"
-                  fi
-                done
-                #Exit
-                exit 0
-            else
-                echo "*********************************************************"
-                echo "       ********************************************      "
-                echo 1>&2 "Spotless Apply found violations it could not fix."
-                echo "Run spotless apply in your terminal and fix the issues before trying to commit again."
-                echo "       ********************************************      "
-                echo "*********************************************************"
-                #Exit
-                exit 1
-            fi
-            """
-        }
-    }
+	providers.exec {
+		commandLine("git", "config", "--worktree", "core.bare", "false")
+	}.result.get()
 
-    commitMsg {
-        conventionalCommits()
-    }
+	providers.exec {
+		commandLine("git", "config", "--worktree", "core.hooksPath", ".githooks")
+	}.result.get()
 
-    createHooks(true)
+	logger.info("Git hooks configured")
+}.onFailure {
+	logger.error("Could not auto-configure Git hooks: ${it.message}")
 }
 
 rootProject.name = "cuttingdelight-1.20.1"
